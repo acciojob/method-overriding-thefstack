78ea33097a1f1dbc0e255484ac68fcc96d90614e
thefstack <rajsharmahwh19@gmail.com>
2024-12-19 20:24:12 +0530

    third

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,30 @@
 package com.driver;
 
-// Task 1: Create class A
-class A {
+
+
+public class Main {
+    // Task 1: Create class A
+public static class A {
     public String meth() {
         return "Invoking method from class A";
     }
 }
 
 // Task 2: Create class B which extends A
-class B extends A {
+public static class B extends A {
     // Task 4: Override method meth of class A
     @Override
     public String meth() {
         return "Method is overridden in Extended class B";
     }
 }
-
-public class Main {
     public static void main(String[] args) {
         // Task 3: Create an object of class B and call method meth of class A
         B objB = new B();
 
         // Call the overridden method
         objB.meth(); // Task 5: Call overridden method
+        B objB1 = new B();
+        objB1.meth();
     }
 }
